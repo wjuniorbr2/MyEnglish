d98d245264f2d388c8beba09d991fa4f6437a458
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-04-30 11:37:05 -0300

    Update app id and version on lesson-list-ui branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.myenglish"
+        applicationId = "br.com.wjunior.myenglish"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
