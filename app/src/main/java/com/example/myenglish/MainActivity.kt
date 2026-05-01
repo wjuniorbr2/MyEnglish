b2a16c265816b9e40d88b41f6d06dafdb5c99a51
Waldomiro <wjuniorbr@hotmail.com>
2026-05-01 01:25:45 -0300

    Move answer helpers to utils package

--- app/src/main/java/com/example/myenglish/MainActivity.kt
+++ app/src/main/java/com/example/myenglish/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.myenglish
 
+import com.example.myenglish.utils.canUseHint
+import com.example.myenglish.utils.cleanAnswer
+import com.example.myenglish.utils.countWords
+import com.example.myenglish.utils.isCorrectAnswer
+import com.example.myenglish.utils.revealedHintText
 import com.example.myenglish.data.HomeworkSentence
 import android.app.Activity
 import android.content.Context
@@ -199,73 +204,6 @@ private object HomeworkData {
     fun hasListeningHomework(lessonName: String): Boolean {
         return lessonName == "Lesson 1" || lessonName == "Lesson 2" || lessonName == "Lesson 3"
     }
-}
-
-fun cleanAnswer(text: String): String {
-    val builder = StringBuilder()
-    var lastWasSpace = true
-    var i = 0
-
-    while (i < text.length) {
-        val currentChar = text[i]
-        val lowerChar = Character.toLowerCase(currentChar)
-
-        if (lowerChar >= 'a' && lowerChar <= 'z') {
-            builder.append(lowerChar)
-            lastWasSpace = false
-        } else if (lowerChar >= '0' && lowerChar <= '9') {
-            builder.append(lowerChar)
-            lastWasSpace = false
-        } else if (currentChar == ' ' || currentChar == '\n' || currentChar == '\t') {
-            if (!lastWasSpace && builder.isNotEmpty()) {
-                builder.append(' ')
-                lastWasSpace = true
-            }
-        }
-
-        i++
-    }
-
-    if (builder.isNotEmpty() && builder[builder.length - 1] == ' ') {
-        builder.deleteCharAt(builder.length - 1)
-    }
-
-    return builder.toString()
-}
-
-fun isCorrectAnswer(studentAnswer: String, correctAnswer: String): Boolean {
-    return cleanAnswer(studentAnswer) == cleanAnswer(correctAnswer)
-}
-
-fun countWords(text: String): Int {
-    val cleanText = cleanAnswer(text)
-    if (cleanText == "") return 0
-    val tokenizer = StringTokenizer(cleanText, " ")
-    return tokenizer.countTokens()
-}
-
-fun revealedHintText(correctText: String, hintCount: Int): String {
-    val cleanText = cleanAnswer(correctText)
-    if (cleanText == "" || hintCount <= 0) return ""
-
-    val tokenizer = StringTokenizer(cleanText, " ")
-    val builder = StringBuilder()
-    var wordsAdded = 0
-
-    while (tokenizer.hasMoreTokens() && wordsAdded < hintCount) {
-        if (builder.isNotEmpty()) builder.append(" ")
-        builder.append(tokenizer.nextToken())
-        wordsAdded++
-    }
-
-    return builder.toString()
-}
-
-fun canUseHint(answer: String, replayCount: Int, submittedAnswers: Boolean, hintCount: Int, correctText: String): Boolean {
-    return submittedAnswers &&
-        cleanAnswer(answer) != "" &&
-        replayCount >= 5 &&
-        hintCount < countWords(correctText)
 }
 
 fun currentDateTimeText(): String {
---
+++ app/src/main/java/com/example/myenglish/utils/AnswerUtils.kt
@@ -0,0 +1,76 @@
+package com.example.myenglish.utils
+
+import java.util.StringTokenizer
+
+fun cleanAnswer(text: String): String {
+    val builder = StringBuilder()
+    var lastWasSpace = true
+    var i = 0
+
+    while (i < text.length) {
+        val currentChar = text[i]
+        val lowerChar = Character.toLowerCase(currentChar)
+
+        if (lowerChar >= 'a' && lowerChar <= 'z') {
+            builder.append(lowerChar)
+            lastWasSpace = false
+        } else if (lowerChar >= '0' && lowerChar <= '9') {
+            builder.append(lowerChar)
+            lastWasSpace = false
+        } else if (currentChar == ' ' || currentChar == '\n' || currentChar == '\t') {
+            if (!lastWasSpace && builder.isNotEmpty()) {
+                builder.append(' ')
+                lastWasSpace = true
+            }
+        }
+
+        i++
+    }
+
+    if (builder.isNotEmpty() && builder[builder.length - 1] == ' ') {
+        builder.deleteCharAt(builder.length - 1)
+    }
+
+    return builder.toString()
+}
+
+fun isCorrectAnswer(studentAnswer: String, correctAnswer: String): Boolean {
+    return cleanAnswer(studentAnswer) == cleanAnswer(correctAnswer)
+}
+
+fun countWords(text: String): Int {
+    val cleanText = cleanAnswer(text)
+    if (cleanText == "") return 0
+    val tokenizer = StringTokenizer(cleanText, " ")
+    return tokenizer.countTokens()
+}
+
+fun revealedHintText(correctText: String, hintCount: Int): String {
+    val cleanText = cleanAnswer(correctText)
+    if (cleanText == "" || hintCount <= 0) return ""
+
+    val tokenizer = StringTokenizer(cleanText, " ")
+    val builder = StringBuilder()
+    var wordsAdded = 0
+
+    while (tokenizer.hasMoreTokens() && wordsAdded < hintCount) {
+        if (builder.isNotEmpty()) builder.append(" ")
+        builder.append(tokenizer.nextToken())
+        wordsAdded++
+    }
+
+    return builder.toString()
+}
+
+fun canUseHint(
+    answer: String,
+    replayCount: Int,
+    submittedAnswers: Boolean,
+    hintCount: Int,
+    correctText: String
+): Boolean {
+    return submittedAnswers &&
+            cleanAnswer(answer) != "" &&
+            replayCount >= 5 &&
+            hintCount < countWords(correctText)
+}
