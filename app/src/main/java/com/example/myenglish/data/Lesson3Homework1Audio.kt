2b944880615a379161933fefa77f76d966d43962
Waldomiro <wjuniorbr@hotmail.com>
2026-05-01 02:01:38 -0300

    Move text helpers to utils package

---
+++ app/src/main/java/com/example/myenglish/data/Lesson3Homework1Audio.kt
@@ -0,0 +1,13 @@
+package com.example.myenglish.data
+
+import com.example.myenglish.R
+
+object Lesson3Homework1Audio {
+    val AUDIO_RES_ID = R.raw.lesson3
+
+    val sentences = arrayOf(
+        HomeworkSentence("Sentence 1", "I like.", 0, 3000),
+        HomeworkSentence("Sentence 2", "You eat meat.", 3000, 6000),
+        HomeworkSentence("Sentence 3", "I don't like.", 6000, 9000)
+    )
+}
