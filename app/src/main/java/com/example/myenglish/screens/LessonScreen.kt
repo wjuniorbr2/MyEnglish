05a04659f6c45488a7dcb3d03063b5529c186cea
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-05-04 00:23:23 -0300

    Enable book for Lesson 3

--- app/src/main/java/com/example/myenglish/screens/LessonScreen.kt
+++ app/src/main/java/com/example/myenglish/screens/LessonScreen.kt
@@ -46,7 +46,7 @@ fun Lesson(
     back: () -> Unit
 ) {
     val listeningAvailable = HomeworkData.hasListeningHomework(name)
-    val bookAvailable = name == "Lesson 1" || name == "Lesson 2"
+    val bookAvailable = name == "Lesson 1" || name == "Lesson 2" || name == "Lesson 3"
 
     Column(
         Modifier
