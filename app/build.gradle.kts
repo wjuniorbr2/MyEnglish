635e33c8ce98b8f2e3c8aaaa740b3a5a15e73468
Waldomiro <wjuniorbr@hotmail.com>
2026-05-07 03:04:18 -0300

    Move StudentBadge to components package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "br.com.wjunior.myenglish"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2"
+        versionCode = 5
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
