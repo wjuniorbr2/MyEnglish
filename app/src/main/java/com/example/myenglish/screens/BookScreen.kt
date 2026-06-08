f6d6faaab51a2a8030381850a0fd820bdbdcba03
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-06-08 16:39:06 -0300

    Replace BookScreen with compact compiling version

--- app/src/main/java/com/example/myenglish/screens/BookScreen.kt
+++ app/src/main/java/com/example/myenglish/screens/BookScreen.kt
@@ -1,22 +1,11 @@
 package com.example.myenglish.screens
 
-import android.media.MediaPlayer
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.speech.tts.TextToSpeech
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.Rounded
+import androidx
