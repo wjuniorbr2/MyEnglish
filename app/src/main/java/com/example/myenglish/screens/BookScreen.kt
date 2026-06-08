0b47fa60fec93815c9de996dcba00e39c1829f27
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-06-08 16:55:54 -0300

    Fix BookScreen function compile

--- app/src/main/java/com/example/myenglish/screens/BookScreen.kt
+++ app/src/main/java/com/example/myenglish/screens/BookScreen.kt
@@ -1,11 +1,20 @@
 package com.example.myenglish.screens
 
 import android.speech.tts.TextToSpeech
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.myenglish.data.*
+import java.util.Locale
+
+@Composable
+fun BookScreen(lessonName: String, back: () -> Unit) {
+    val data = when (lessonName) {
+       
