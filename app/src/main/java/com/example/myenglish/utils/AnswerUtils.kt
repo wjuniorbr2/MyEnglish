5b5060e546b8873cc8aae6be10a00e5e9d6e7308
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-06-09 16:45:48 -0300

    Restore isCorrectAnswer helper

--- app/src/main/java/com/example/myenglish/utils/AnswerUtils.kt
+++ app/src/main/java/com/example/myenglish/utils/AnswerUtils.kt
@@ -9,11 +9,4 @@ fun cleanAnswer(text: String): String {
         .replace("thanks", "thank you")
         .replace("don't", "do not")
         .replace("doesn't", "does not")
-        .replace("i'm", "i am")
-        .replace("you're", "you are")
-        .replace("it's", "it is")
-        .replace("that's", "that is")
-        .replace(Regex("[^a-z0-9 ]"), " ")
-        .replace(Regex("\\s+"), " ")
-        .trim()
-}
+        .replace
