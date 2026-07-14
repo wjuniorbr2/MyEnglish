01fae7987eedc04c671384fad85ae5219ee30bf1
wjuniorbr2 <wjuniorbr@hotmail.com>
2026-07-14 19:08:58 -0300

    Use compatible activity transition APIs

--- app/src/main/java/com/example/myenglish/MainActivity.kt
+++ app/src/main/java/com/example/myenglish/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myenglish
 
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,11 +11,13 @@ import com.example.myenglish.ui.theme.MyEnglishTheme
 import com.example.myenglish.utils.prepareSplashWindow
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        applyNoTransition()
         enableEdgeToEdge()
         prepareSplashWindow(window)
-        overridePendingTransition(0, 0)
 
         setContent {
             MyEnglishTheme {
@@ -24,6 +28,32 @@ class MainActivity : ComponentActivity() {
 
     override fun finish() {
         super.finish()
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            applyLegacyNoTransition()
+        }
+    }
+
+    private fun applyNoTransition() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            overrideActivityTransition(
+                Activity.OVERRIDE_TRANSITION_OPEN,
+                0,
+                0
+            )
+
+            overrideActivityTransition(
+                Activity.OVERRIDE_TRANSITION_CLOSE,
+                0,
+                0
+            )
+        } else {
+            applyLegacyNoTransition()
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun applyLegacyNoTransition() {
         overridePendingTransition(0, 0)
     }
 }
